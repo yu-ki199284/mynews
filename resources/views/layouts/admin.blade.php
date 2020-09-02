fd712ae7b0ff32d0f6325110f6cddf68259fa908
”yuuki <y9045895@gmail.com>
2020-09-02 11:18:17 +0000

    init

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,48 @@
+<!DOCTIME html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <title>@yield('title')</title>
+        
+        <script src="{{ secure_asset('js/app.js') }}" defar></script>
+        
+        <link rel="dns-prefetch" href="http://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+        
+        <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+        
+    </head>
+    <body>
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+                <div class="container">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        {{ config('app.name', 'laravel') }}
+                    </a>
+                    <button class="navbar-toggler" type="button" date-toggle="collapse" date-target="#navbarSupportedcontent" aria-controls="navbarSupportedcontent" aria-expanted="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    
+                    <div class="collapse navbar-collapse" id="navbarSupportedcontent">
+                        <ul class="navbar-nav mr-auto">
+                            
+                        </ul>
+                        
+                        <ul class="navbar-nav ml-auto">
+                            
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <main class="py-4">
+                @yield('content')
+            </main>
+        </div>
+    </body>
+</html>
