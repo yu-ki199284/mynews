238927b241777fd83dd313f1e0dc5a762d349f9a
”yuuki <y9045895@gmail.com>
2020-09-07 13:46:10 +0000

    コンパイル

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE-edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>My Create作成画面</h1>
-    </body>
-</html>
+@extends('layouts.profile')
+
+@section('title','プロフィールの新規作成')
+
+@section('content')
+     <div class="container">
+         <div class="row">
+             <div class="col-md-8 mx-auto">
+                 <h2>プロフィール新規作成画面</h2>
+             </div>
+         </div>
+     </div>
+@endsection     
