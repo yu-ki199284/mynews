0fa33472f455f9badead70fb1fe8d5ec20b36612
”yuuki <y9045895@gmail.com>
2020-09-08 14:25:45 +0000

    Auth実装

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function(){
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 //PHP/Laravel09 Routingを理解する。 ↓課題３，４
@@ -30,3 +30,10 @@ Route::group(['prefix' => 'admin'],function(){
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
