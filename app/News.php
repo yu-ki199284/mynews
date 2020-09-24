b33c05cda24ae6126cc5bdf2ad43ffc13773cd1e
”yuuki <y9045895@gmail.com>
2020-09-24 14:42:57 +0000

    14章　投稿データを保存しよ

---
+++ app/News.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $guarded = array('id');
+    public static $rules = array(
+        'title' => 'recuired',
+        'body' => 'recuired',
+        );
+}
