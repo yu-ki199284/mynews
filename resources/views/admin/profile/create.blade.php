aba0d9b4265a87aa30ced5a417ab213df688d704
”yuuki <y9045895@gmail.com>
2020-10-08 04:52:02 +0000

    16章　課題3~4

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -39,12 +39,6 @@
                             <textarea class="form-control" name="introduction" rows="20">{{ old('introduction') }}</textarea>
                         </div>
                     </div>
-                    <div class="form-group-row">
-                        <label class="col-md-2">イメージ画像</label>
-                        <div class="col-md-10">
-                            <input type="file" class="form-control-file" name="image">
-                        </div>
-                    </div>
                     {{ csrf_field() }}
                     <input type="submit" class="btn btn-primary" value="新規作成">
                 </form>
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,12 +1,62 @@
 @extends('layouts.profile')
 
-@section('title','Yukis HomePage')
+@section('title','プロフィールの編集')
 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>I AM YU-KI</h2>
+                <h2>プロフィール編集</h2>
+                <form action="{{ action('Admin\ProfileController@update') }}" method="post" enctype="multipart/form-date">
+                    @if(count($errors) > 0)
+                    <ul>
+                        @foreach($errors->all() as $e)
+                        <li>{{ $e }}</li>
+                        @endforeach
+                    </ul>
+                    @endif
+                    <div class="form-group row">
+                        <lavel class="col-md-2" for="name">名前</lavel>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <lavel class="col-md-2" for="gender">性別</lavel>
+                        <div class="col-md-10">
+                           <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender }}"> 
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <lavel class="col-md-2" for="hobby">趣味</lavel>
+                        <div class="col-md-10">
+                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="introduction">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-10">
+                            <input type="submit" class="btn btn-primary" value="更新">
+                        </div>
+                    </div>
+                </form>
+                <div class="row mt-5">
+                    <div class="col-md-4 mx-auto">
+                        <h2>更新履歴</h2>
+                        <ul class="list-group">
+                            @if ($profile_form->profile_histories !=NULL)
+                            @foreach ($profile_form->profile_histories as $profile_history)
+                            <li class="list-group-item">{{ $profile_history->edited_at }}</li>
+                            @endforeach
+                            @endif
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
