bcce90485867619d8b75e8a9a11ab56f6f60adc3
”yuuki <y9045895@gmail.com>
2020-11-05 12:58:04 +0000

    画像のアップロード

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -11,6 +11,8 @@ use App\History;
 
 use Carbon\Carbon;
 
+use storage; //追加
+
 class NewsController extends Controller
 {
     public function add()
@@ -26,8 +28,8 @@ class NewsController extends Controller
         $form = $request->all();
         
         if (isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news->image_path = null;
         }
@@ -72,8 +74,8 @@ class NewsController extends Controller
         if($request->remove == 'true') {
             $news_form['image_path'] = null;
         } elseif($request->file('image')) {
-            $path = $request->file('image')->store('public/image');
-            $news_form['image_path'] = basename($path);
+            $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+            $news_form['image_path'] =  Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
         }
